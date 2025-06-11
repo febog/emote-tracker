c9a3ef25d4f7a8df6fe171b5ea97eab3f25fb919
Felipe Bojorquez <felipe@febog.com>
2025-06-10 20:08:35 -0700

    Seed DB with empty app access token

--- EmoteTracker/EmoteTracker/Data/SeedData.cs
+++ EmoteTracker/EmoteTracker/Data/SeedData.cs
@@ -9,30 +9,39 @@ namespace EmoteTracker.Data
         {
             using (var context = new EmoteTrackerContext(serviceProvider.GetRequiredService<DbContextOptions<EmoteTrackerContext>>()))
             {
-                if (context == null || context.EmoteServices == null)
+                if (context == null || context.EmoteServices == null || context.TwitchAppAccessTokens == null)
                 {
                     throw new ArgumentNullException("Null EmoteTrackerContext");
                 }
 
-                if (context.EmoteServices.Any())
+                if (!context.EmoteServices.Any())
                 {
-                    return; // DB has been seeded
+                    context.EmoteServices.AddRange(
+                        new EmoteService
+                        {
+                            Name = "FFZ"
+                        },
+                        new EmoteService
+                        {
+                            Name = "BTTV"
+                        },
+                        new EmoteService
+                        {
+                            Name = "7TV"
+                        });
                 }
 
-                context.EmoteServices.AddRange(
-                    new EmoteService
-                    {
-                        Name = "FFZ"
-                    },
-                    new EmoteService
-                    {
-                        Name = "BTTV"
-                    },
-                    new EmoteService
-                    {
-                        Name = "7TV"
-                    }
-                );
+                if (!context.TwitchAppAccessTokens.Any())
+                {
+                    context.TwitchAppAccessTokens.AddRange(
+                        new TwitchAppAccessToken
+                        {
+                            AccessToken = "token",
+                            ExpiresIn = 0,
+                            TokenType = "bearer"
+                        });
+                }
+
                 context.SaveChanges();
             }
         }
