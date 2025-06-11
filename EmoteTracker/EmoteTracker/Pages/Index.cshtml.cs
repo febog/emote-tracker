ebcd6d09f3bbee864e02df13190eb048910c4736
Felipe Bojorquez <felipe@febog.com>
2025-06-11 00:15:06 -0700

    Add initial home layout

--- EmoteTracker/EmoteTracker/Pages/Index.cshtml.cs
+++ EmoteTracker/EmoteTracker/Pages/Index.cshtml.cs
@@ -1,4 +1,3 @@
-using EmoteTracker.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,29 +5,9 @@ namespace EmoteTracker.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly IFrankerService _emoteService;
-        private readonly ITwitchService _twitchService;
-
-        public IndexModel(ILogger<IndexModel> logger,
-            IFrankerService emoteService,
-            ITwitchService twitchService)
+        public IActionResult OnGet()
         {
-            _logger = logger;
-            _emoteService = emoteService;
-            _twitchService = twitchService;
-        }
-
-        [BindProperty(SupportsGet = true)]
-        public string Username { get; set; }
-
-        public IList<ChannelEmote> Emotes { get; set; }
-
-        public async Task OnGetAsync()
-        {
-            var userId = await _twitchService.GetTwitchId(Username);
-            Emotes = await _emoteService.GetChannelEmotes(userId);
-            if (Emotes == null) { Emotes = new List<ChannelEmote>(); }
+            return Page();
         }
     }
 }
