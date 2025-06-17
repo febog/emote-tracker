cb3f42900bba06a13dcc6ad92c64f75f35ffec0b
Felipe Bojorquez <felipe@febog.com>
2025-06-16 23:06:28 -0700

    Fix 7TV emote page URL

--- EmoteTracker/EmoteTracker/Services/SevenEmote.cs
+++ EmoteTracker/EmoteTracker/Services/SevenEmote.cs
@@ -4,7 +4,7 @@
     {
         public override ChannelEmoteType EmoteType => ChannelEmoteType.SevenEmote;
 
-        public override string EmotePage => "https://cdn.7tv.app/emote/" + Id;
+        public override string EmotePage => "https://7tv.app/emotes/" + Id;
 
         public override string ImageUrl => "https://cdn.7tv.app/emote/" + Id + "/1x.avif";
     }
