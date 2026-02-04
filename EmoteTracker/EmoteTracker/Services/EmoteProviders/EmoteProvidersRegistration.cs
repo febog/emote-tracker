9d4c96cc087042bbde18e813d5abe123341bda7f
Felipe Bojorquez <felipe@febog.com>
2026-02-03 23:01:41 -0800

    Order providers alphabetically

--- EmoteTracker/EmoteTracker/Services/EmoteProviders/EmoteProvidersRegistration.cs
+++ EmoteTracker/EmoteTracker/Services/EmoteProviders/EmoteProvidersRegistration.cs
@@ -8,8 +8,8 @@ namespace EmoteTracker.Services.EmoteProviders
     {
         public static IServiceCollection RegisterEmoteProviders(this IServiceCollection services)
         {
-            services.AddHttpClient<IFrankerService, FrankerService>(client => client.BaseAddress = new Uri("https://api.frankerfacez.com/v1/room/id/"));
             services.AddHttpClient<IBttvService, BttvService>(client => client.BaseAddress = new Uri("https://api.betterttv.net/3/cached/users/twitch/"));
+            services.AddHttpClient<IFrankerService, FrankerService>(client => client.BaseAddress = new Uri("https://api.frankerfacez.com/v1/room/id/"));
             services.AddHttpClient<ISevenService, SevenService>(client => client.BaseAddress = new Uri("https://7tv.io/v3/users/twitch/"));
 
             return services;
