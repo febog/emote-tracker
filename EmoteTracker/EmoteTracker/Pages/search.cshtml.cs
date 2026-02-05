eceb7193b424b2b56415ad0f0ce48c1e83b05ac0
Felipe Bojorquez <felipe@febog.com>
2026-02-04 21:40:26 -0800

    Clean dead code

--- EmoteTracker/EmoteTracker/Pages/search.cshtml.cs
+++ EmoteTracker/EmoteTracker/Pages/search.cshtml.cs
@@ -1,10 +1,9 @@
-using EmoteTracker.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EmoteTracker.Pages
 {
-    public class SearchModel() : PageModel
+    public class SearchModel : PageModel
     {
         public async Task<IActionResult> OnGetAsync(string channel)
         {
