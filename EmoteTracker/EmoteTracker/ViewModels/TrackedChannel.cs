9dafd769663eda9e2c0565a4ddacdc7ec70764aa
Felipe Bojorquez <felipe@febog.com>
2026-02-05 21:04:00 -0800

    Add ViewModels

---
+++ EmoteTracker/EmoteTracker/ViewModels/TrackedChannel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmoteTracker.ViewModels
+{
+    public class TrackedChannel
+    {
+        [Display(Name = "TWITCH_CHANNEL_ID")]
+        public string ChannelId { get; set; }
+
+        [Display(Name = "TWITCH_CHANNEL_DISPLAY_NAME")]
+        public string DisplayName { get; set; }
+
+        public ICollection<TrackedEmote> TrackedEmotes { get; set; }
+    }
+}
---
+++ EmoteTracker/EmoteTracker/ViewModels/TrackedEmote.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmoteTracker.ViewModels
+{
+    public class TrackedEmote
+    {
+        public string Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string CanonicalName { get; set; }
+
+        public string Alias { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public bool IsListed { get; set; }
+
+        [Display(Name = "Type")]
+        public EmoteProvider Provider { get; }
+
+        public string EmotePage { get; }
+
+        public string ImageUrl { get; }
+    }
+
+    public enum EmoteProvider
+    {
+        // Do not reorder
+        Unknown = 0,
+        [Display(Name = "FFZ")]
+        FrankerEmote = 1,
+        [Display(Name = "BTTV")]
+        BttvEmote = 2,
+        [Display(Name = "7TV")]
+        SevenEmote = 3
+    }
+}
